068ebd30fcab08a868d71c87a1026256ff550100
CristianVelazquez1995 <58026968+CristianVelazquez1995@users.noreply.github.com>
2020-12-26 12:23:12 -0300

    a

---
+++ src/components/App.js
@@ -0,0 +1,56 @@
+import React, { useState } from "react";
+import Accordion from "./Accordion";
+import Search from "./Search";
+import Dropdown from "./Dropdown";
+
+const items = [
+  {
+    title: "What is reacts?",
+    content: "React is a front end javascript framework",
+  },
+  {
+    title: "Why use React?",
+    content: "React is a favorite Js library",
+  },
+  {
+    title: "How do you use React?",
+    content: "You use React by creating components",
+  },
+];
+
+const options = [
+  {
+    label: "The color Red",
+    value: "red",
+  },
+  {
+    label: "The color Green",
+    value: "green",
+  },
+  {
+    label: "The color Blue",
+    value: "blue",
+  },
+];
+
+const App = () => {
+  const [selected, setSelected] = useState(options[0]);
+  const [showDropdown, setShowDropdown] = useState(true);
+
+  return (
+    <div>
+      <button onClick={() => setShowDropdown(!showDropdown)}>
+        Toggle Dropdown
+      </button>
+      {showDropdown ? (
+        <Dropdown
+          options={options}
+          selected={selected}
+          onSelection={setSelected}
+        />
+      ) : null}
+    </div>
+  );
+};
+
+export default App;
